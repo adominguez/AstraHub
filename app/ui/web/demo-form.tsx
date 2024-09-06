162c2ec677adb47db54232c276e47c20ce0b7e6a
Adominguez <dmartin.alberto@gmail.com>
2024-09-06 15:56:01 +0200

    chore: improve button styling in demo form

--- app/ui/web/demo-form.tsx
+++ app/ui/web/demo-form.tsx
@@ -122,7 +122,9 @@ export default function DemoForm() {
           }
           <button
             className='inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2'
-            onClick={() => setStatus(STATUS.FORM)}>pasar al anterior</button>
+            onClick={() => setStatus(STATUS.FORM)}>
+              pasar al anterior
+          </button>
         </div>
       </div>
     </div>
